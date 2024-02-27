a7f6fc678af059952ff9184a23b6cdd0a752f76b
SuJin <kimsujin001127@gmail.com>
2024-02-27 19:57:38 +0900

    feat: 기본 컴포넌트 구성

--- wanted-todolist/src/App.tsx
+++ wanted-todolist/src/App.tsx
@@ -1,5 +1,22 @@
+import styled from "styled-components";
+import Input from "./components/Input/Input";
+import Button from "./components/Button/Button";
+import List from "./components/List/List";
+
 function App() {
-  return <></>;
+  return (
+    <Container>
+      <HeadBox>
+        <Input />
+        <Button />
+      </HeadBox>
+      <List />
+    </Container>
+  );
 }
+
+const Container = styled.div``;
+
+const HeadBox = styled.div``;
 
 export default App;
